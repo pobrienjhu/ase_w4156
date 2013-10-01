725c9f71975099808674c8739cd024a0ee05b65a
Phil Ames <pa2354@columbia.edu>
2013-09-30 23:18:56 -0400

    Test repository commit.

--- ASE - TeamProject/src/config/data.sql
+++ ASE - TeamProject/src/config/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO Persons values (default, 'OBrien', 'Patrick',  '123', 'NY', 'po22207@columbia.edu');
 INSERT INTO Persons values (default, 'Nijjar', 'Aiman',  '234', 'NY', 'an2434@columbia.edu');
-INSERT INTO Persons values (default, 'Ames', 'Phil',  '345', 'NY', 'pa2354@columbia.edu');
+INSERT INTO Persons values (default, 'Ames', 'Phil',  '345', 'NY', 'pa2354+w4156@columbia.edu');
 INSERT INTO Persons values (default, 'Abramson', 'Reva',  '456', 'NY', 'ra2659@columbia.edu');
 INSERT INTO Persons values (default, 'Spahn', 'Reily',  '567', 'NY', 'reily@cs.columbia.edu');
