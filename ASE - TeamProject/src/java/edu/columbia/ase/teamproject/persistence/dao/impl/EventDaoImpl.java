7eef35787607e69c6b7a7a51210a1f0a7958ad16
pobrienjhu <patrick.obrien.8474@gmail.com>
2013-10-21 23:00:32 -0400

    first cut of event sql private event sql not working yet

--- ASE - TeamProject/src/java/edu/columbia/ase/teamproject/persistence/dao/impl/EventDaoImpl.java
+++ ASE - TeamProject/src/java/edu/columbia/ase/teamproject/persistence/dao/impl/EventDaoImpl.java
@@ -1,6 +1,5 @@
 package edu.columbia.ase.teamproject.persistence.dao.impl;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.joda.time.DateTime;
@@ -10,13 +9,39 @@ import org.springframework.transaction.annotation.Transactional;
 import edu.columbia.ase.teamproject.persistence.dao.EventDao;
 import edu.columbia.ase.teamproject.persistence.dao.generic.HibernateDao;
 import edu.columbia.ase.teamproject.persistence.domain.Event;
+import edu.columbia.ase.teamproject.persistence.domain.enumeration.EventType;
 
 @Transactional(propagation = Propagation.REQUIRED)
 public class EventDaoImpl extends HibernateDao<Event, Long> implements EventDao {
 
+	@SuppressWarnings("unchecked")
 	@Override
-	public Collection<Event> getAllPublicEvents(DateTime start, DateTime end){
-		return new ArrayList<Event>();
+	public Collection<Event> getAllPublicEvents(DateTime currentTime){
+		
+		return currentSession().createQuery(
+				"From Event e "+
+				"Where e.eventType = :et "+
+				"and :ct between e.startTime and e.endTime  ")
+				.setParameter("et", EventType.PUBLIC )
+				.setParameter("ct", currentTime ).list();				
 	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Collection<Event> getAllPrivateEventsForUserId(DateTime currentTime,
+			Long userId) {
+		
+		return currentSession().createQuery(
+				"From Event e "+
+				"inner join e.eventUsers eventUsers "+ 
+				"Where e.eventType = :et "+
+				"and eventUsers.id = :uId "+
+				"and :ct between e.startTime and e.endTime ")
+				.setParameter("uId", userId )
+				.setParameter("et", EventType.PRIVATE )
+				.setParameter("ct", currentTime ).list();	
+		
+	}
+	
 	
 }
