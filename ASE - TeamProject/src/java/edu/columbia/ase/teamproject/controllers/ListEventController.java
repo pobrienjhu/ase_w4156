64a1c95623ce46fa25b3edeacbc80e9567d3b9e7
Phil Ames <pa2354@columbia.edu>
2013-10-26 23:35:05 -0400

    Fix XSS flaw + change default event type to public

--- ASE - TeamProject/src/java/edu/columbia/ase/teamproject/controllers/ListEventController.java
+++ ASE - TeamProject/src/java/edu/columbia/ase/teamproject/controllers/ListEventController.java
@@ -1,10 +1,13 @@
 package edu.columbia.ase.teamproject.controllers;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.StringUtils;
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.google.gson.Gson;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import edu.columbia.ase.teamproject.persistence.dao.UserAccountDao;
+import edu.columbia.ase.teamproject.persistence.domain.Event;
 import edu.columbia.ase.teamproject.persistence.domain.UserAccount;
 import edu.columbia.ase.teamproject.services.EventService;
-import edu.columbia.ase.teamproject.util.GsonProvider;
 
 @Controller
 @RequestMapping("/app/listEvent.do")
@@ -32,16 +36,24 @@ public class ListEventController {
 	@Autowired
 	private UserAccountDao userAccountDao;
 
-	@Autowired
-	GsonProvider gsonProvider;
-
 	private static final Logger logger = LoggerFactory
 			.getLogger(ListEventController.class);
 
+	private List<Map<String, Object>> buildEventDataList(Collection<Event> events) {
+		List<Map<String, Object>> rv = Lists.newArrayList();
+
+		for (Event e: events) {
+			Map<String, Object> eventData = Maps.newHashMap();
+			eventData.put("id", e.getId().toString());
+			eventData.put("name", e.getName());
+			eventData.put("description", e.getDescription());
+			rv.add(eventData);
+		}
+		return rv;
+	}
+
 	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView doGet(HttpSession session) {
-		
-		Gson gson = gsonProvider.provideGson();
 
 		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().
 				getAuthentication().getPrincipal();
@@ -55,6 +67,15 @@ public class ListEventController {
 		
 		model.put("userName", displayName );
 		model.put("title", "Event List");
+
+		Collection<Event> currentPublicEvents =
+				eventService.getAllActivePublicEvents(DateTime.now());
+		Collection<Event> currentPrivateEvents =
+				eventService.getAllActivePrivateEventsForUserId(DateTime.now(),
+						user.getId());
+
+		model.put("publicEventData", buildEventDataList(currentPublicEvents));
+		model.put("privateEventData", buildEventDataList(currentPrivateEvents));
 
 		return new ModelAndView("soy:edu.columbia.ase.event.listEvent", model);
 		
