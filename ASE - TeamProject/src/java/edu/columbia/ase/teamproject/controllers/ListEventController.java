26cdeb63a01d6f74580b425601e2d95ece32fbeb
Phil Ames <pa2354@columbia.edu>
2013-10-27 10:31:11 -0400

    Restore async behavior of listEvent page

--- ASE - TeamProject/src/java/edu/columbia/ase/teamproject/controllers/ListEventController.java
+++ ASE - TeamProject/src/java/edu/columbia/ase/teamproject/controllers/ListEventController.java
@@ -1,13 +1,10 @@
 package edu.columbia.ase.teamproject.controllers;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang.StringUtils;
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-
 import edu.columbia.ase.teamproject.persistence.dao.UserAccountDao;
-import edu.columbia.ase.teamproject.persistence.domain.Event;
 import edu.columbia.ase.teamproject.persistence.domain.UserAccount;
 import edu.columbia.ase.teamproject.services.EventService;
 
@@ -39,19 +32,6 @@ public class ListEventController {
 	private static final Logger logger = LoggerFactory
 			.getLogger(ListEventController.class);
 
-	private List<Map<String, Object>> buildEventDataList(Collection<Event> events) {
-		List<Map<String, Object>> rv = Lists.newArrayList();
-
-		for (Event e: events) {
-			Map<String, Object> eventData = Maps.newHashMap();
-			eventData.put("id", e.getId().toString());
-			eventData.put("name", e.getName());
-			eventData.put("description", e.getDescription());
-			rv.add(eventData);
-		}
-		return rv;
-	}
-
 	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView doGet(HttpSession session) {
 
@@ -67,15 +47,6 @@ public class ListEventController {
 		
 		model.put("userName", displayName );
 		model.put("title", "Event List");
-
-		Collection<Event> currentPublicEvents =
-				eventService.getAllActivePublicEvents(DateTime.now());
-		Collection<Event> currentPrivateEvents =
-				eventService.getAllActivePrivateEventsForUserId(DateTime.now(),
-						user.getId());
-
-		model.put("publicEventData", buildEventDataList(currentPublicEvents));
-		model.put("privateEventData", buildEventDataList(currentPrivateEvents));
 
 		return new ModelAndView("soy:edu.columbia.ase.event.listEvent", model);
 		
